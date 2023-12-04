46e15410a70c1669113a6b7d3d8f23c3ce9362b4
k7elite <karichubonface@yahoo.com>
2023-12-04 22:18:57 +0300

    just remerbered it

---
+++ public/index.53253e6e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".nav-toggle"),n=document.querySelectorAll(".nav__link");e.addEventListener("click",()=>{document.body.classList.toggle("nav-open")}),n.forEach(e=>{e.addEventListener("click",()=>{document.body.classList.remove("nav-open")})});
+//# sourceMappingURL=index.53253e6e.js.map
